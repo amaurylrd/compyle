32a7e1f2e711f00ca935c89abdc62d0bc6a71b4a
amaurylrd <amaurylrd@yahoo.fr>
2025-06-13 14:22:00 +0200

    fix: replace deprecated pkg_resources w/ importlib.metadata

--- compyle/__init__.py
+++ compyle/__init__.py
@@ -1,7 +1,7 @@
-import pkg_resources
+from importlib.metadata import version
 
 from compyle.celery import app as celery_app
 
-__version__ = pkg_resources.get_distribution(__package__).version
+__version__ = version(__package__)
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 __all__ = ("celery_app",)
